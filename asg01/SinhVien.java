3b651a6f78a4305d898cda6107cb196011d46956
dieupv <hnd.dieupham@gmail.com>
2014-07-29 21:22:29 +0700

    no message

---
+++ asg01/SinhVien.java
@@ -0,0 +1,13 @@
+public class SinhVien{
+    public static void main(String[] args){
+	       System.out.println("Pham Van Dieu 11020076 K56CD dieupv hnd.dieupham@gmail.com");
+		   //int i;
+           for(int i=9;i>1;i--){
+               System.out.println("bottles of beer on the wall,"+i);
+			   System.out.println("bottles of beer. Take one down, pass it around, ");
+           }
+	  
+	}
+}	  
+
+
